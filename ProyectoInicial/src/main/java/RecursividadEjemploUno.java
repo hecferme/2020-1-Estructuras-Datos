e6e39bd10b22ba9ac1a194b8938ae7b6225c368d
LAB01 <LAB01@PC-PROFESOR>
2020-01-24 19:18:03 -0600

    Clase de Main con clase auxiliar para recursividad

---
+++ ProyectoInicial/src/main/java/RecursividadEjemploUno.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB01
+ */
+public class RecursividadEjemploUno {
+    
+    private void CapturarString(){
+    
+    }
+    
+    private void ImprimirDeDerechaAIzquierda()
+    {
+            }
+    
+    private void ImprimirRecursivo ()
+    {
+        
+    }
+    
+    public void EjecutarEjemploUno ()
+    {
+        CapturarString ();
+        ImprimirDeDerechaAIzquierda ();
+        
+    }
+
+}
